3f35be6a88a7995e5dda3f361a2abc7ff6e184a5
Snehaps <snehaps0278@gmail.com>
2026-07-31 22:11:00 +0530

    Remove tracked .pyc files and update project configuration

--- fm_backend/config/settings.py
+++ fm_backend/config/settings.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5174",
+#     "http://localhost:5173",
 # ]
 
 # URLs & Templates
